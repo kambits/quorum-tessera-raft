ae9b0774d47a9c176f43dac71307b7d5e67b993c
Bruce Lee <coolcode@live.com>
2020-01-16 12:42:18 +0800

    :monkey_face: join

--- c-restart.sh
+++ c-restart.sh
@@ -19,4 +19,6 @@ echo '[2] Restart Tessera.'
 java -jar tessera.jar -configfile $qd/config.json >> $qd/logs/tessera.log 2>&1 &
 sleep 1
 
+geth --datadir $qd/dd init $qd/genesis.json
+sleep 1
 PRIVATE_CONFIG=$qd/tm.ipc geth --datadir $qd/dd --networkid 99999 --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcport "$[$n+22000]" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --port "$[$n+30300]" --nodiscover --unlock 0 --raftport "$[$n+50400]" --verbosity 4 --password $qd/passwords.txt --miner.gaslimit 18446744073709551615 --miner.gastarget 18446744073709551615 --raftblocktime 250 1>$qd/logs/geth.log 2>$qd/logs/geth.log &
