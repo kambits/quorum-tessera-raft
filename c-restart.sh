29d02d148077e8a0d37748e9f765db402ff1c4fb
Bruce Lee <coolcode@live.com>
2020-01-16 12:51:01 +0800

    :monkey_face:

--- c-restart.sh
+++ c-restart.sh
@@ -7,8 +7,7 @@ echo '#################################'
 n=1
 qd=qd
 
-echo '[1] Stop Quorum and Tessera.'
-killall -9 geth
+echo '[1] Stop Tessera.'
 killall -9 java
 
 sleep 10
@@ -18,7 +17,3 @@ rm -rf $qd/tm.ipc
 echo '[2] Restart Tessera.'
 java -jar tessera.jar -configfile $qd/config.json >> $qd/logs/tessera.log 2>&1 &
 sleep 1
-
-geth --datadir $qd/dd init $qd/genesis.json
-sleep 1
-PRIVATE_CONFIG=$qd/tm.ipc geth --datadir $qd/dd --networkid 99999 --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcport "$[$n+22000]" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --port "$[$n+30300]" --nodiscover --unlock 0 --raftport "$[$n+50400]" --verbosity 4 --password $qd/passwords.txt --miner.gaslimit 18446744073709551615 --miner.gastarget 18446744073709551615 --raftblocktime 250 1>$qd/logs/geth.log 2>$qd/logs/geth.log &
