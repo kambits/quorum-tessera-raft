48a8a39f60b4fd6df9519ec0757350e22c269c10
Bruce Lee <coolcode@live.com>
2020-01-20 18:30:34 +0800

    :monkey_face: set raft id

--- c-start.sh
+++ c-start.sh
@@ -32,6 +32,7 @@ echo '[4] Start quorum.'
 PRIVATE_CONFIG=$qd/tm.ipc geth --datadir $qd/dd --networkid 99999 --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcport "$[$n+22000]" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --port "$[$n+30300]" --nodiscover --unlock 0 --raftport "$[$n+50400]" --verbosity 4 --password $qd/passwords.txt --miner.gaslimit 18446744073709551615 --miner.gastarget 18446744073709551615 --raftblocktime 250 1>$qd/logs/geth.log 2>$qd/logs/geth.log &
 sleep 1
 
+echo "1" > $qd/RAFT_ID
 
 
 echo '################################'
